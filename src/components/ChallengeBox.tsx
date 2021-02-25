df75d38c5775e982bb1ea11f187e68f290311bf3
Patrick <patrickvieiramota@gmail.com>
2021-02-25 00:29:16 -0300

    Create ChallengeBox component

---
+++ src/components/ChallengeBox.tsx
@@ -0,0 +1,42 @@
+import styles from '../styles/components/ChallengeBox.module.css'
+
+export function ChallengeBox(){
+    const hasActiveChallenge = true
+    return (
+        <div className={styles.challengeBoxContainer}>
+            {
+                hasActiveChallenge ? (
+                    <div className={styles.challengeActive}>
+                        <header>Ganhe 400</header>
+
+                        <main>
+                            <img src="icons/body.svg" alt="Desafio"/>
+                            <strong>Novo desafio</strong>
+                            <p>Levante e beba água</p>
+                        </main>
+
+                        <footer>
+                            <button
+                                type="button"
+                                className={styles.challengeFailedButton}
+                            >Falhei</button>
+                            <button
+                                type="button"
+                                className={styles.challengeSucceededButton}
+                            >Completei</button>
+                        </footer>
+                    </div>
+                ) : (
+                    <div className={styles.challengeNotActive}>
+                        <strong>Finalize um ciclo para receber um desafio</strong>
+                        <p>
+                            <img src="icons/level-up.svg" alt="Level up"/>
+                            Avance de level completando os desafio
+                        </p>
+                    </div>
+                )
+            }
+            
+        </div>
+    )
+}
